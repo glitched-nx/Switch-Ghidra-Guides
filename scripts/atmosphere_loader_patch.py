c1531ab1015910c8f0be2e626a9a56cade95d5d9
Peter Galonza <p.galonza@evaron.ru>
2024-06-30 11:46:12 +0300

    refactor: refactor logs and formatting strings to improve understanding

--- scripts/atmosphere_loader_patch.py
+++ scripts/atmosphere_loader_patch.py
@@ -66,20 +66,32 @@ def main():
             logger_interface.error('Instructions for the patch were not found')
             sys.exit(0)
         hash = hashlib.sha256(loader_kip).hexdigest().upper()
-        logger_interface.info('\nIPS LOADER HASH: %s\nIPS LOADER PATCH: %s\nHEKATE LOADER HASH: %s\nHEKATE LOADER PATCH: %04X:0x1:01,00',
-                                hash, patch, hash[:16], result.end()-0x100)
 
-        with open('./patches/atmosphere/kip_patches/loader_patches/%s.ips' % hash, 'wb') as ips_file:
-            ips_file.write(bytes.fromhex(str(f'5041544348{patch}454F46')))
-        logger_interface.info('%s.ips OK', hash)
-
+        offset_hex_0x = f'0x{result.end()-0x100:04X}'
+        length_hex_0x = f'0x{1:X}'
         decompressed_loader_kip.seek(result.end())
+        src_data_hex = decompressed_loader_kip.read(0x1).hex().upper()
+        dst_data_hex='00'
+        logger_interface.info('\nHEKATE LOADER HASH: %s\nHEKATE LOADER OFFSET: %s',
+                              hash[:16], offset_hex_0x)
         with open('./hekate_patches/loader_patches.ini', 'a') as loader_hekate:
             loader_hekate.write('\n')
             loader_hekate.write(f'#Loader Atmosphere-{atmosphere_version}-{atmosphere_hash}\n')
             loader_hekate.write(f'[Loader:{hash[:16]}]\n')
-            loader_hekate.write(f'.nosigchk=0:0x{result.end()-0x100:04X}:0x1:{decompressed_loader_kip.read(0x1).hex().upper()},00\n')
+            loader_hekate.write(f'.nosigchk=0:{offset_hex_0x}:{length_hex_0x}:{src_data_hex},{dst_data_hex}\n')
         logger_interface.info('loader_patches.ini OK')
+
+
+        ips_header = b'PATCH'.hex()
+        ips_offset = f'{result.end():06X}'
+        ips_data_size = f'{1:04X}'
+        ips_data = '00'
+        ips_footer = b'EOF'.hex()
+        logger_interface.info('\nIPS LOADER HASH: %s\nIPS LOADER OFFSET: %s',
+                              hash, ips_offset)
+        with open('./patches/atmosphere/kip_patches/loader_patches/%s.ips' % hash, 'wb') as ips_file:
+            ips_file.write(bytes.fromhex(ips_header + ips_offset + ips_data_size + ips_data + ips_footer))
+        logger_interface.info('%s.ips OK', hash)
 
     with open('./patches/bootloader/patches.ini', 'wb') as outfile:
         for filename in ['./hekate_patches/header.ini', './hekate_patches/fs_patches.ini', './hekate_patches/loader_patches.ini']:
