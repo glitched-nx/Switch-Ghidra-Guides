d14286ca72bd29fd0b23ecc97bce7b44d9ff9e43
Peter Galonza <p.galonza@evaron.ru>
2024-07-06 00:42:19 +0300

    feature: add check key-file function

--- scripts/modules/__init__.py
+++ scripts/modules/__init__.py
@@ -16,6 +16,44 @@ def get_build_id(file_object):
     return(file_object.read(0x14).hex().upper())
 
 
+def check_key_file(file_path):
+    result = True
+    valid_hashes = {
+        'mariko_bek':'dac25c717109976b8dac45d9184e9c11',
+        'mariko_kek': '4a4eea6d1a812ee7aa1dfd9a3f71bc27',
+        'master_key_00': 'c069db652c88b7edc4f31127a3331678',
+    }
+    ns_keys = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            key, value = line.replace(' ', '').rstrip().split('=')
+            ns_keys[key] = value
+
+    for p_key in ns_keys.keys():
+        if 'package1_key_' in p_key:
+            break
+    else:
+        logger_interface.error('package1_key_<> is missing in keys file, we cannot proceed with keygen as package1 cannot be opened for the purpose of obtaining master_kek_source. Exiting.')
+        result = False
+
+    for p_key in ns_keys.keys():
+        if 'tsec_root_key_' in p_key:
+            break
+    else:
+        logger_interface.error('tsec_root_key_<> is missing in keys file, we cannot derive a new master_kek from the new master_kek_source, keygen will not yield new keys. Exiting.')
+        result = False
+
+    for p_key, p_value_hash in valid_hashes.items():
+        t_hash = hashlib.md5(ns_keys[p_key].encode('utf-8')).hexdigest()
+        if p_value_hash != t_hash:
+            logger_interface.error('hash is not valid for %s value', p_key)
+            result = False
+            break
+
+    return result
+
+
+
 if platform.system() == "Windows":
     hactoolnet = "./tools/hactoolnet-windows.exe"
     hactool = "./tools/hactool-windows.exe"
