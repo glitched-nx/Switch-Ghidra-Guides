8e4a1e6aa914878c2a9d5dd3ab8cea83d9b13152
Peter Galonza <p.galonza@evaron.ru>
2024-07-06 00:47:55 +0300

    style: pep8 new line

--- scripts/modules/__init__.py
+++ scripts/modules/__init__.py
@@ -28,6 +28,7 @@ def logging_configuration(logger):
     logger.setLevel(logging.DEBUG)
     logger.addHandler(sh)
 
+
 def get_build_id(file_object):
     file_object.seek(0x40)
     return(file_object.read(0x14).hex().upper())
